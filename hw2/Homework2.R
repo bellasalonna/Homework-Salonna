d3d8cfb7cff103dab5bd1491766f72288e7b602b
Isabella Salonna <is9400a@student.american.edu>
2024-02-14 10:42:10 -0500

    Commiting R file

---
+++ hw2/Homework2.R
@@ -0,0 +1,17 @@
+
+# Isabella Salonna
+# HW 2
+
+library(tidyverse)
+
+# 1
+library(tidyverse)
+tribble( ~x, ~y, ~w, ~z,
+         210, 300, 220, 180,
+         102, 100, 119, 187,
+         176, 175, 188, 173,
+         87, 95, 91, 94,
+         202, 210, 234, 218,
+         110, 122, 131, 128,
+) -> dt
+dt
